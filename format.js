57557965e4dfda175e8e3eaedae54e673c67750f
GiovanniGCastagna <giovanni.gcastagna@gmail.com>
2024-08-07 20:53:33 -0300

    Arquivos JS

---
+++ format.js
@@ -0,0 +1,8 @@
+let a = 5
+let b = 3
+console.log('Divisão simples =', a / b, 'teste');
+console.log(`Divisão Simples = ${a / b} teste`);
+
+console.log('Divisão fixed =', (a / b).toFixed(2), 'teste');
+console.log(`Divisão fixed = ${(a / b).toFixed(3)} teste`);
+console.log(`Divisão fixed = ${(a / b).toFixed(4)} teste`);
---
+++ somas.js
@@ -0,0 +1,5 @@
+let a = 1;
+let b = 5;
+console.log("soma de a + b =", (a + b).toString());
+console.log("soma de 1 + 1 =", 1 + 1);
+console.log(`Divisão final = ${(5 / 2)} tlg?`);
